38acf06da1d34c938283caf6414339c27e1e9477
Tran Kim Hieu <trankimhieu567@gmail.com>
2024-09-07 18:36:40 +0700

    pagination work, table is state controlable, fix bug

--- src/components/Table/Pagination.js
+++ src/components/Table/Pagination.js
@@ -1,46 +1,76 @@
-import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
-const Pagination = ({ totalItems, rowsPerPage }) => {
-  if (rowsPerPage && typeof rowsPerPage === 'number') {
-    console.log('this is number 09282');
-    const totalPages = Math.ceil(totalItems / rowsPerPage);
-    const pageNumbers = [];
-
-    pageNumbers.push(
-      <Link
-        to="#"
-        className="relative inline-flex items-center px-4 py-2 text-sm font-medium leading-5 text-gray-700 transition duration-150 ease-in-out bg-white border rounded-md cursor-pointer border-fuchsia-100 hover:bg-fuchsia-200 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10"
-      >
-        Previous
-      </Link>,
-    );
-    for (let i = 1; i <= totalPages; i++) {
-      pageNumbers.push(
-        <Link
-          to="#"
-          className="relative inline-flex items-center px-4 py-2 text-sm font-medium leading-5 text-gray-700 transition duration-150 ease-in-out bg-white border rounded-md cursor-pointer border-fuchsia-100 hover:bg-fuchsia-200 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10"
-        >
-          {i}
-        </Link>,
-      );
-    }
-    pageNumbers.push(
-      <Link
-        to="#"
-        className="relative inline-flex items-center px-4 py-2 text-sm font-medium leading-5 text-gray-700 transition duration-150 ease-in-out bg-white border rounded-md cursor-pointer border-fuchsia-100 hover:bg-fuchsia-200 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10"
-      >
-        next
-      </Link>,
-    );
-
-    return (
-      <nav class="flex space-x-2 ml-[15%]" aria-label="Pagination">
-        {pageNumbers}
-      </nav>
-    );
+const Pagination = ({ totalItems, rowsPerPage, currentPage, handleSetCurrentPage }) => {
+  if (!rowsPerPage && typeof rowsPerPage !== 'number') {
+    return null; // Handle the case where rowPerPage is not valid
   }
 
-  return null; // Handle the case where rowPerPage is not valid
+  const totalPages = Math.ceil(totalItems / rowsPerPage);
+  const pageNumbers = [];
+
+  const goToNextPage = () => {
+    if (currentPage < totalPages) {
+      handleSetCurrentPage(currentPage + 1);
+    }
+  };
+
+  const goToPreviousPage = () => {
+    if (currentPage > 1) {
+      handleSetCurrentPage(currentPage - 1);
+    }
+  };
+
+  currentPage > 1
+    ? pageNumbers.push(
+        <button
+          to="#"
+          className={
+            'relative inline-flex items-center px-4 py-2 text-sm font-medium leading-5 transition duration-150 ease-in-out border rounded-md cursor-pointer border-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 ' +
+            (currentPage === 1
+              ? 'text-gray-300 cursor-not-allowed'
+              : 'text-gray-700 hover:border-sky-500 hover:text-sky-500')
+          }
+          onClick={goToPreviousPage}
+        >
+          Previous
+        </button>,
+      )
+    : pageNumbers.push(<div className="w-[88px]"></div>);
+  for (let i = 1; i <= totalPages; i++) {
+    pageNumbers.push(
+      <button
+        to="#"
+        className={
+          'relative inline-flex items-center px-4 py-2 text-sm font-medium leading-5 transition duration-150 ease-in-out  border rounded-md cursor-pointer border-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 ' +
+          (currentPage === i
+            ? 'bg-sky-500 text-white cursor-default'
+            : 'text-gray-700 hover:border-sky-500 hover:text-sky-500')
+        }
+        onClick={() => handleSetCurrentPage(i)}
+      >
+        {i}
+      </button>,
+    );
+  }
+  currentPage < totalPages
+    ? pageNumbers.push(
+        <button
+          to="#"
+          className={
+            'relative inline-flex items-center px-4 py-2 text-sm font-medium leading-5 transition duration-150 ease-in-out  border rounded-md cursor-pointer border-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 focus:z-10 text-gray-700 hover:border-sky-500 hover:text-sky-500'
+          }
+          onClick={goToNextPage}
+        >
+          next
+        </button>,
+      )
+    : pageNumbers.push(<div className="w-[62px]"></div>);
+
+  return (
+    <nav class="flex space-x-2 ml-[15%]" aria-label="Pagination">
+      {pageNumbers}
+    </nav>
+  );
 };
 
 export default Pagination;
--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -5,12 +5,68 @@
   + actions: array component, chứa component 
   + rowsPerPage:  number
 */
-
+import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Pagination from './Pagination';
 import { faScrewdriverWrench, faTrash } from '@fortawesome/free-solid-svg-icons';
 
 function Table({ headers, rows, actions, rowsPerPage }) {
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const tableRow = (value) => {
+    return (
+      <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white" key={value}>
+        {value}
+      </td>
+    );
+  };
+  const renderTableRowsByPage = (currentPage, rowsPerPage) => {
+    const elements = [];
+    const startIndex = (currentPage - 1) * rowsPerPage;
+    let endIndex = startIndex + rowsPerPage;
+    if (endIndex > rows.length) {
+      endIndex = rows.length;
+    }
+
+    for (let i = startIndex; i < endIndex; i++) {
+      const row = rows[i];
+      const rowCells = [];
+
+      const values = Object.values(row);
+      for (let j = 0; j < values.length; j++) {
+        rowCells.push(tableRow(values[j]));
+      }
+
+      elements.push(
+        <tr class="hover:bg-gray-100 dark:hover:bg-gray-700" key={i}>
+          {rowCells}
+          <td class="flex flex-row py-4 px-6 gap-2">
+            <FontAwesomeIcon icon={faTrash} style={{ color: '#e61433' }} size="4xs" />
+            <FontAwesomeIcon icon={faScrewdriverWrench} style={{ color: '#74C0FC' }} size="4xs" />
+          </td>
+        </tr>,
+      );
+    }
+
+    return elements;
+  };
+
+  const renderAllTableRow = () => {
+    return rows.map((row, index) => (
+      <tr class="hover:bg-gray-100 dark:hover:bg-gray-700" key={index}>
+        {Object.values(row).map((value) => (
+          <td class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white" key={value}>
+            {value}
+          </td>
+        ))}
+        <td class="flex flex-row py-4 px-6 gap-2">
+          <FontAwesomeIcon icon={faTrash} style={{ color: '#e61433' }} size="4xs" />
+          <FontAwesomeIcon icon={faScrewdriverWrench} style={{ color: '#74C0FC' }} size="4xs" />
+        </td>
+      </tr>
+    ));
+  };
+
   return (
     <>
       <div class="overflow-x-auto shadow-md sm:rounded-lg m-2">
@@ -38,22 +94,11 @@ function Table({ headers, rows, actions, rowsPerPage }) {
               </thead>
               <tbody class="bg-white divide-y divide-gray-200 dark:bg-gray-800 dark:divide-gray-700">
                 {rows ? (
-                  rows.map((row, index) => (
-                    <tr class="hover:bg-gray-100 dark:hover:bg-gray-700" key={index}>
-                      {Object.values(row).map((value) => (
-                        <td
-                          class="py-4 px-6 text-sm font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                          key={value}
-                        >
-                          {value}
-                        </td>
-                      ))}
-                      <td class="flex flex-row py-4 px-6 gap-2">
-                        <FontAwesomeIcon icon={faTrash} style={{ color: '#e61433' }} size="4xs" />
-                        <FontAwesomeIcon icon={faScrewdriverWrench} style={{ color: '#74C0FC' }} size="4xs" />
-                      </td>
-                    </tr>
-                  ))
+                  rowsPerPage ? (
+                    renderTableRowsByPage(currentPage, rowsPerPage)
+                  ) : (
+                    renderAllTableRow()
+                  )
                 ) : (
                   <div className="h-20"></div>
                 )}
@@ -62,7 +107,14 @@ function Table({ headers, rows, actions, rowsPerPage }) {
           </div>
         </div>
       </div>
-      {rowsPerPage && <Pagination totalItems={rows.length} rowsPerPage={rowsPerPage} />}
+      {rowsPerPage && (
+        <Pagination
+          totalItems={rows.length}
+          rowsPerPage={rowsPerPage}
+          currentPage={currentPage}
+          handleSetCurrentPage={setCurrentPage}
+        />
+      )}
     </>
   );
 }
