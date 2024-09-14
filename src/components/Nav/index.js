523d1841dbd86b37beba1199a7f2d6446d0492f2
nvhoang2652 <nvhoang2605@gmail.com>
2024-09-14 22:23:03 +0700

    create nav admin

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -2,10 +2,19 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-function Nav({ icon, name, href, color, isActive }) {
+function Nav({ icon, name, href, color, isActive, number, isAdmin }) {
   const [iconColor, setIconColor] = useState(color || '');
   const [textColor, setTextColor] = useState('text-stone-600');
   const [isHovered, setIsHovered] = useState(false);
+
+  if (isAdmin) {
+    return (
+      <div className={`w-3/12 min-h-20 ${color} flex flex-col rounded-sm p-3`}>
+        <label className="font-normal text-sm ">{name}</label>
+        <span className="font-semibold text-4xl	mt-2">{number}</span>
+      </div>
+    );
+  }
 
   return (
     <li
--- src/pages/Admin/index.js
+++ src/pages/Admin/index.js
@@ -1,5 +1,14 @@
+import Nav from '@/components/Nav';
+
 function Admin() {
-  return <h1>hello</h1>;
+  return (
+    <div className="flex justify-between mt-4 gap-5">
+      <Nav name="Phòng đến trong ngày" number="0" isAdmin color="bg-sky-100" />
+      <Nav name="Phòng đi trong ngày" number="12" isAdmin color="bg-violet-100" />
+      <Nav name="Phòng đang sử dụng" number="10" isAdmin color="bg-emerald-100" />
+      <Nav name="Khách đang ở" number="24" isAdmin color="bg-orange-100" />
+    </div>
+  );
 }
 
 export default Admin;
